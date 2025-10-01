09e8a7a4e10adc2b842c628ac677f80a8c94fd5b
Pasha_stenkt <selina@fredic.ch>
2025-10-01 15:39:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        // JavaScript zum Abspielen des Sounds und Vergrößern des Bildes
+// JavaScript zum Abspielen des Sounds und Vergrößern des Bildes
         document.getElementById('imageButton').addEventListener('click', function() {
             var audio = document.getElementById('audioElement');
             audio.play();
@@ -6,4 +6,8 @@
             // Bild vergrößern
             this.classList.add('clicked');
 
-               });
+            // Optional: Klasse nach einer kurzen Zeit wieder entfernen, um den Effekt rückgängig zu machen
+            setTimeout(() => {
+                this.classList.remove('clicked');
+            }, 300);  // 300 Millisekunden
+        });

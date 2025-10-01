26584da975f78bbf6d3365b8912833afd95fd867
Pasha_stenkt <selina@fredic.ch>
2025-10-01 14:31:10 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+        // JavaScript zum Abspielen des Sounds und Vergrößern des Bildes
+        document.getElementById('imageButton').addEventListener('click', function() {
+            var audio = document.getElementById('audioElement');
+            audio.play();
+            
+            // Bild vergrößern
+            this.classList.add('clicked');
+
+            // Optional: Klasse nach einer kurzen Zeit wieder entfernen, um den Effekt rückgängig zu machen
+            setTimeout(() => {
+                this.classList.remove('clicked');
+            }, 300);  // 300 Millisekunden
+        });
